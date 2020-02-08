cf191b8c62d619178a99d92c5222c9336d75f24a
Ben Spinks <32001471+spinks@users.noreply.github.com>
2020-02-08 16:20:47 +0000

    Update command line program, doesn't yet handle flags

--- gameoflife.c
+++ gameoflife.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include "gol.h"
+
 int main(int argc, char* argv[]) {
+  FILE* infile = stdin;
+  FILE* outfile = stdout;
+  int generations = 5;
+  int statistics = 0;
+  int torus = 0;
+
   struct universe v;
-  read_in_file(stdin, &v);
-  evolve(&v, will_be_alive);
-  evolve(&v, will_be_alive);
-  evolve(&v, will_be_alive);
-  evolve(&v, will_be_alive);
-  evolve(&v, will_be_alive);
+  v.alive_cumulative = 0;
+  v.total_cumulative = 0;
+
+  read_in_file(infile, &v);
+  for (int i = 0; i != generations; i++) {
+    if (statistics == 1) {
+      print_statistics(&v);
+    }
+    if (torus == 1) {
+      evolve(&v, will_be_alive_torus);
+    } else {
+      evolve(&v, will_be_alive);
+    }
+  }
   write_out_file(stdout, &v);
   return 0;
 }

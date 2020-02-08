22bd0e12825701e38afb60176ba18b9e14acf244
Ben Spinks <32001471+spinks@users.noreply.github.com>
2020-02-08 16:37:35 +0000

    Basic flag handling

--- gameoflife.c
+++ gameoflife.c
@@ -8,9 +8,32 @@ int main(int argc, char* argv[]) {
   int statistics = 0;
   int torus = 0;
 
-  struct universe v;
-  v.alive_cumulative = 0;
-  v.total_cumulative = 0;
+  for (int i = 1; i != argc; i++) {
+    if (argv[i][0] == '-') {
+      switch (argv[i][1]) {
+        default:
+          printf("Unknown option -%c\n\n", argv[i][1]);
+          break;
+        case 'i':
+          infile = fopen(argv[i + 1], "r");
+          break;
+        case 'o':
+          outfile = fopen(argv[i + 1], "w");
+          break;
+        case 'g':
+          generations = atoi(argv[i + 1]);
+          break;
+        case 's':
+          statistics = 1;
+          break;
+        case 't':
+          torus = 1;
+          break;
+      }
+    }
+  }
+
+  struct universe v = {.alive_cumulative = 0, .total_cumulative = 0};
 
   read_in_file(infile, &v);
   for (int i = 0; i != generations; i++) {
@@ -23,6 +46,6 @@ int main(int argc, char* argv[]) {
       evolve(&v, will_be_alive);
     }
   }
-  write_out_file(stdout, &v);
+  write_out_file(outfile, &v);
   return 0;
 }
